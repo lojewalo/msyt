257e4c1cd1f92d892f501e6fb1cb22288cd51cd9
Anna <git@anna.lgbt>
2019-03-05 00:36:55 -0500

    feat: add --directories for windows users with inferior globbing

--- src/cli.rs
+++ src/cli.rs
@@ -15,12 +15,24 @@ pub fn app<'a, 'b: 'a>() -> App<'a, 'b> {
     .subcommand(SubCommand::with_name("import")
       .about("Import from MSYT files to MSBT files")
 
+      .arg(Arg::with_name("dir_mode")
+        .help("Allow specifying directories. msyt will search for all files with the correct extension in the provided directories.")
+        .short("d")
+        .long("directories")
+        .alias("directory"))
+
       .arg(Arg::with_name("paths")
         .help("MSYT paths to import (MSBT files should be adjacent)")
         .required(true)
         .multiple(true)))
     .subcommand(SubCommand::with_name("export")
       .about("Export from MSBT files to MSYT files")
+
+      .arg(Arg::with_name("dir_mode")
+        .help("Allow specifying directories. msyt will search for all files with the correct extension in the provided directories.")
+        .short("d")
+        .long("directories")
+        .alias("directory"))
 
       .arg(Arg::with_name("paths")
         .help("MSBT paths to export")
