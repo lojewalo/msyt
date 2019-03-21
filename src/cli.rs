b159fac4bff7d3d22472f6a935ca1f1199dd8878
Kyle Clemens <git@kyleclemens.com>
2019-03-21 14:37:30 -0400

    fix: use args for import, not export

--- src/cli.rs
+++ src/cli.rs
@@ -21,19 +21,6 @@ pub fn app<'a, 'b: 'a>() -> App<'a, 'b> {
         .long("directories")
         .alias("directory"))
 
-      .arg(Arg::with_name("paths")
-        .help("MSYT paths to import (MSBT files should be adjacent)")
-        .required(true)
-        .multiple(true)))
-    .subcommand(SubCommand::with_name("export")
-      .about("Export from MSBT files to MSYT files")
-
-      .arg(Arg::with_name("dir_mode")
-        .help("Allow specifying directories. msyt will search for all files with the correct extension in the provided directories.")
-        .short("d")
-        .long("directories")
-        .alias("directory"))
-
       .arg(Arg::with_name("no-backup")
         .help("Do not create a backup of any existing output files")
         .short("B")
@@ -47,6 +34,19 @@ pub fn app<'a, 'b: 'a>() -> App<'a, 'b> {
         .alias("ext")
         .takes_value(true)
         .default_value("msbt"))
+
+      .arg(Arg::with_name("paths")
+        .help("MSYT paths to import (MSBT files should be adjacent)")
+        .required(true)
+        .multiple(true)))
+    .subcommand(SubCommand::with_name("export")
+      .about("Export from MSBT files to MSYT files")
+
+      .arg(Arg::with_name("dir_mode")
+        .help("Allow specifying directories. msyt will search for all files with the correct extension in the provided directories.")
+        .short("d")
+        .long("directories")
+        .alias("directory"))
 
       .arg(Arg::with_name("paths")
         .help("MSBT paths to export")
