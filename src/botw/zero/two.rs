8e9d880540d3eec538bf9993396a62a4d84f61b9
Kyle Clemens <git@kyleclemens.com>
2019-03-23 12:23:41 -0400

    feat: add text size control

--- src/botw/zero/two.rs
+++ src/botw/zero/two.rs
@@ -16,8 +16,8 @@ use std::io::{Cursor, Write};
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Control0_2 {
-  field_1: u16,
-  field_2: u16,
+  pub field_1: u16,
+  pub field_2: u16,
 }
 
 impl SubControl for Control0_2 {
@@ -26,9 +26,16 @@ impl SubControl for Control0_2 {
   }
 
   fn parse(header: &Header, mut reader: &mut Cursor<&[u8]>) -> Result<Control> {
+    let field_1 = header.endianness().read_u16(&mut reader).with_context(|_| "could not read field_1")?;
+    let field_2 = header.endianness().read_u16(&mut reader).with_context(|_| "could not read field_2")?;
+
+    if field_1 == 2 {
+      return Ok(Control::TextSize { percent: field_2 });
+    }
+
     Ok(Control::Raw(RawControl::Zero(Control0::Two(Control0_2 {
-      field_1: header.endianness().read_u16(&mut reader).with_context(|_| "could not read field_1")?,
-      field_2: header.endianness().read_u16(&mut reader).with_context(|_| "could not read field_2")?,
+      field_1,
+      field_2,
     }))))
   }
 
